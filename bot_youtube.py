208331ae950fc91d6533f31695a557ab6982cd16
Joe Colino <joepc74@gmail.com>
2026-04-15 09:06:19 +0200

    cambio de parametro cookies

--- bot_youtube.py
+++ bot_youtube.py
@@ -60,7 +60,7 @@ async def handle_message(client, message):
         }
         # si existe el archivo de cookies, lo añadimos a las opciones de yt-dlp para manejar sesiones autenticadas
         if os.path.exists("cookie.txt"):
-            ydl_opts['cookiefile'] = 'cookie.txt'
+            ydl_opts['cookies'] = 'cookie.txt'
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
         filename = ydl.prepare_filename(info)
